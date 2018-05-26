b58093a7e2cdf5abeabd377f50e0f434814eff12
547483792 <547483792@qq.com>
2018-05-26 22:49:52 +0800

    the initial version

---
+++ src/com.main/Test.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+public class Test {
+	public static void main(String[] args) {
+		
+		int a=0;
+		int b=Integer.parseInt("1236");
+		char[] cr="my name is Chen".toCharArray();
+		String str=String.valueOf(cr);
+
+
+//		try {
+//		a=System.in.read();
+//		}
+//		catch (IOException e)
+//		{
+//			System.out.printf(e.toString());
+//		}
+		System.out.print(Integer.toString(b));
+         //jj
+
+
+	}
+
+}
